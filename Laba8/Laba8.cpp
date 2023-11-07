59839a4ffdadb3546d4890c40d625b5a954e848c
Anton Kazantsev <146850404+TuchkaSoul@users.noreply.github.com>
2023-11-07 23:12:45 +0500

    Add files via upload

---
+++ Laba8/Laba8.cpp
@@ -0,0 +1,199 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+
+struct Information
+{
+	char carrier[255]; //Носитель
+	char volume[255]; //Объём
+	char title[255]; //Название
+	char author[255]; //Автор
+};
+
+char file_start[] = "test_start.txt";
+int n;
+
+
+void createFile()
+{
+	FILE* file;
+
+	if ((file = fopen(file_start, "wb")) == NULL)
+	{
+		cout << "Ошибка открытия файла" << endl;
+		return;
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		Information information1;
+		cin.get();
+		cout << " * Введите носитель: ";
+		cin.getline(information1.carrier, 255);
+		cout << " * Введите объём: ";
+		cin.getline(information1.volume, 255);
+		cout << " * Введите название: ";
+		cin.getline(information1.title, 255);
+		cout << " * Введите автора: ";
+		cin.get(information1.author, 255);
+
+		cout << endl;
+
+		fwrite(&information1, sizeof(Information), 1, file);
+	}
+	fclose(file);
+	return;
+}
+
+void WriteFile()
+{
+	FILE* file;
+
+	if ((file = fopen(file_start, "rb")) == NULL)
+	{
+		cout << "Ошибка открытия файла";
+		return;
+	}
+
+	cout << "Вывод файла: " << endl;
+	for(int i = 1; i <= n and !feof(file); i++)
+	{
+		Information information;
+		fread(&information, sizeof(Information), 1, file);
+
+		cout << " < Носитель: " << information.carrier << endl;
+		cout << " < Объем: " << information.volume << endl;
+		cout << " < Название: " << information.title << endl;
+		cout << " < Адрес: " << information.author << endl;
+		
+		cout << endl;
+	}
+	fclose(file);
+	return;
+}
+
+
+int deleteElement()
+{
+	FILE* file1;
+	FILE* file2;
+	int delElement = 0;
+	string delVolume;
+	cout << "Введите объем информации для удаления: ";
+	cin >> delVolume;
+	cout << endl;
+
+	char file_temp[] = "test_temp.txt";
+
+	if ((file1 = fopen(file_start, "rb")) == NULL)
+	{
+		cout << "Ошибка открытия файла";
+		return 0;
+	}
+
+	if ((file2 = fopen(file_temp, "wb")) == NULL)
+	{
+		cout << "Ошибка открытия файла";
+		return 0;
+	}
+
+	for (int i = 1; i <= n and !feof(file1); i++)
+	{
+		Information information;
+		fread(&information, sizeof(Information), 1, file1);
+
+		if (information.volume != delVolume or delElement>=1)
+		{
+			fwrite(&information, sizeof(Information), 1, file2);
+		}
+		else
+		{
+			delElement += 1;
+		}
+	}
+	
+	fclose(file1);
+	fclose(file2);
+
+	remove(file_start);
+	rename(file_temp, file_start);
+	return delElement;
+}
+
+int addElement()
+{
+	FILE* file1;
+	FILE* file2;
+
+	int addElement = 0;
+	int addNum;
+	cout << "Введите номер элемента, перед которым хотите добавить информацию : ";
+	cin >> addNum;
+	cout << endl;
+	
+	char file_temp[] = "test_temp.txt";
+
+	if ((file1 = fopen(file_start, "rb")) == NULL)
+	{
+		cout << "Ошибка открытия файла";
+		return 0;
+	}
+
+	if ((file2 = fopen(file_temp, "wb+")) == NULL)
+	{
+		cout << "Ошибка открытия файла";
+		return 0;
+	}
+	for (int i = 1; i < n+1; i++)
+	{
+		Information information;
+		fread(&information, sizeof(Information), 1, file1);
+		if (i == addNum) {
+			Information new_information;
+			cin.get();
+			cout << " * Введите носитель: ";
+			cin.getline(new_information.carrier, 255);
+			cout << " * Введите объём: ";
+			cin.getline(new_information.volume, 255);
+			cout << " * Введите название: ";
+			cin.getline(new_information.title, 255);
+			cout << " * Введите автора: ";
+			cin.get(new_information.author, 255);
+			cout << endl;
+
+			fwrite(&new_information, sizeof(Information), 1, file2);
+		}fwrite(&information, sizeof(Information), 1, file2);
+	}
+	
+	fclose(file1);
+	fclose(file2);
+
+	remove(file_start);
+	rename(file_temp, file_start);
+
+	return 1;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(0, "");
+
+	cout << "Лабораторная работа 8\nВведите количество информации: ";
+	cin >> n;
+	cout << endl;
+
+	createFile();
+
+	WriteFile();
+
+	n-= deleteElement();
+	WriteFile();
+
+	n+= addElement();
+	WriteFile();
+	
+	return 0;
+}
---
+++ Laba9/Laba9.cpp
@@ -0,0 +1,143 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include<Windows.h>
+#include <regex> 
+
+
+char F1_char[] = "F1.txt";
+char F2_char[] = "F2.txt";
+int numStrings = 100;
+const int sizeStrings = 1003;
+std::string MinWord() {
+    setlocale(0, "rus");
+    std::string line;
+    std::ifstream in(F2_char);
+    std::string minWord;
+    int min = -1;
+    if (in.is_open())
+    {
+        while (std::getline(in, line)) 
+        {
+            std::regex words_regex("\\b\\w+");
+            auto words_begin = std::sregex_iterator(line.begin(), line.end(), words_regex);
+            char* resalt = new char[distance(words_begin, std::sregex_iterator())];
+
+
+            
+            for (std::sregex_iterator k = words_begin; k != std::sregex_iterator(); ++k) 
+            {
+                std::smatch match = *k;
+                std::string match_str = match.str();
+                if (match_str.length() < min or min == -1) 
+                {
+                    min = match_str.length();
+                    minWord = match_str;
+                }
+            }
+        }
+    }
+    in.close();
+    std::ofstream out;          
+    out.open(F2_char, std::ios::app);      
+    if (out.is_open())
+    {
+        out << "\nThe shortest word equals: " << minWord << std::endl;
+    }
+    out.close();
+    return minWord;
+}
+bool CreateFile1() 
+{
+    FILE* F1; char mass;
+	if ((F1 = fopen(F1_char, "wb")) == NULL)
+	{
+		std::cout << "Ошибка открытия файла" << std::endl;
+		return false;
+	}
+    for (int i = 0; i < numStrings; i++) 
+    {
+        for (int i = 0; i < sizeStrings; i++)
+        {
+            mass = (rand() % ('z' - 'a' + 2)) + 'a' - 1;
+            if (mass < 'a') mass = ' ';
+            fwrite(&mass, 1, sizeof(mass), F1);
+        }
+        mass = '\n';
+        fwrite(&mass, 1, sizeof(mass), F1);
+    }
+    fclose(F1);
+    return true;
+}
+char* GetFirstCharString(char* strings) {
+    std::string strg = strings;
+    std::regex words_regex("\\b\\w");
+    auto words_begin = std::sregex_iterator(strg.begin(), strg.end(), words_regex);
+    char* resalt = new char[distance(words_begin, std::sregex_iterator())];
+    int i = 0;
+    for (std::sregex_iterator k = words_begin; k != std::sregex_iterator(); ++k) {
+        std::smatch match = *k;
+        std::string match_str = match.str();
+        char* c = new char[2];
+        std::copy(match_str.begin(), match_str.end(), c);
+        resalt[i++] = c[0];
+        delete[] c;
+    }
+    return resalt;
+}
+bool isFirstOriginal(char* str) {
+    if (*str != '\0') {
+        char firstChar = *str;
+        bool isOriginal = true;
+        str++;
+        while (*str != '\0' && isOriginal) {
+            isOriginal = firstChar != *str;
+            str++;
+        }
+        return isOriginal;
+    }
+    else {
+        return false;
+    }
+}
+bool CreateFile2()
+{
+    setlocale(0, "rus");
+    FILE* F1;
+    FILE* F2;
+
+    if ((F1 = fopen(F1_char, "rb")) == NULL)
+    {
+        std::cout << "Ошибка открытия файла" << std::endl;
+        return false;
+    }
+    if ((F2 = fopen(F2_char, "wb")) == NULL)
+    {
+        std::cout << "Ошибка открытия файла" << std::endl;
+        return false;
+    }
+    char buf[sizeStrings+1];
+    while (fgets(buf, sizeStrings+1,F1) != NULL)
+    {
+
+        if (isFirstOriginal(GetFirstCharString(buf))) {
+            fwrite(buf, sizeStrings, 1, F2);
+            fwrite("\n", 1, 1, F2);
+        }
+            
+    }
+    fclose(F1);
+    fclose(F2);
+    return true;
+}
+
+
+int main()
+{
+    setlocale(0, "rus");
+    std::cout << "Лабораторная работа 9\n";
+    CreateFile1();
+    CreateFile2();
+    MinWord();
+}
+
